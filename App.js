1adc92233e2f665e2fca0a9cb727a08c9c517f8f
Grzegorz Tomaka (245669) <grzesiektomaka@gmail.com>
2020-04-21 00:27:31 +0200

    initial_screen

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import InitialScreen from './src/screens/InitialScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+const navigator = createStackNavigator({  
+  Initial: InitialScreen
+
+}, {
+  initialRouteName: 'Initial',
+  defaultNavigationOptions: {
+    headerShown: false,
+    cardStyle: { backgroundColor: '#FDC856' }
+  }  
 }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(navigator);
---
+++ src/screens/InitialScreen.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import womanImage from '../data/images/woman.png'
+
+
+const InitialScreen = () =>{
+    return(
+        <View style={styles.backgroundStyle}>
+            <Text style={styles.headerTxt}>SweetBaby</Text>
+            <Image source={womanImage}/>
+            <TouchableOpacity style={styles.startBtn}>
+                <Text style={styles.startTxt}>START</Text>
+            </TouchableOpacity>
+        </View>
+    );
+} 
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    headerTxt: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        bottom: 15
+    },
+    startBtn: {
+        backgroundColor: '#EC6162',
+        borderRadius: 32,
+        paddingHorizontal: 40,
+        paddingVertical: 12,
+        top: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    startTxt:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFFFFF'
+    }
+});
+
+export default InitialScreen
